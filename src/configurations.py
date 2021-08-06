20355d79a780eaaa432f6af005388f43a730960c
reinaqu <reinaqu@gmail.com>
2021-08-06 17:11:01 +0200

    Configurations

---
+++ src/configurations.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 6 ago. 2021
+
+@author: reinaqu_2
+'''
+import commons
+
+searches_conf = {'ACM': [0, 6, 7],
+                       'Springer': [1, 8, 9],
+                       'Science Direct': [2, 12, 13],
+                       'IEEE': [3,10,11],
+                       'Scopus':[4,14,15],
+                       'Google Academic': [5,16,17],
+                       'Snowballing':[n for n in range(18,185)]}
+config_publ ={'skip_rows': 7, 
+             'sheet_name':'Papers',
+             'use_cols':[0,1,2,3,4,9,10,11,12,13,14,15,16,17,18,19],
+             'type': 'Type',
+             'doi':'DOI',
+             'year':'Year',
+             'id_start': 'ID Paper',
+             'title': 'Title',
+             'id_search': 'ID Search Session',
+             'searches_datasources': commons.invert_dict(searches_conf),
+             'authors':'Authors',
+             'publication-type':'Type',
+             'venue':'Journal',
+             'keywords':'Keywords'}
+    
+config_auth ={'author_ID':'Author ID',
+                  'author_name':'Author name',
+                  'id_start':'Paper ID',
+                  'sheet_name':'authors',
+                  'country':'Country',
+                  'skip_rows':0}
+
+config_venues ={'venue':'Venue',
+                    'id_start':'Paper ID',
+                    'type':'Type',
+                    'sheet_name':'venues',
+                    'skip_rows':0}
+
+    
+    
+config_data_extraction ={'skip_rows': 7, 
+             'sheet_name':'Papers',
+             'use_cols':[0,1]+[n for n in range(35,133)],
+             'id_start': 'ID Paper',
+             'title': 'Title',
+             }
