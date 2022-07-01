22611c2e98020f4870540654007f968fa1031de5
reinaqu <reinaqu@gmail.com>
2022-07-01 21:23:23 +0200

    Added ordering in mostrar_dict, and rename to show_dict

--- src/test_utils.py
+++ src/test_utils.py
@@ -13,18 +13,33 @@ V = TypeVar('V')
 
 
 
-def mostrar_dict(d: Dict[str, Set[str]]):
-     for k,v in sorted(d.items()):
+def show_dict(d: Dict[str, Set[str]]):
+    ## sorted by number of elements of the set (descending) and the key, ascending
+    ordered = sorted(d.items(), key=lambda it:(-len(it[1]), it[0]))
+    for k,v in sorted(ordered, reverse=True):
         print (k, '-->', list(v))
+        
+def show_dict_latex(d:Dict[str,Set[str]]):
+    ## sorted by number of elements of the set (descending) and the key, ascending
+    ordered = sorted(d.items(), key=lambda it:(-len(it[1]), it[0]))
+    for key, value in ordered:
+        str_value = ", ".join(map(lambda s:f"\cite{{{s:}}}", value))
+        print(f"{key}\n\t{str_value}") 
 
 def show_quality_bubble_plot(quality_file:str, datadash:datextdash.DashboardDataExtraction):
     pub_quality = pubq.PublicationsQuality.of_excel(quality_file, configurations.config_publ)  
     datadash.set_publications_quality(pub_quality)
-    print(pub_quality.count_pairs_per_quality_measure)
-    datadash.create_bubble_quality()
-    
+    #print(pub_quality.count_pairs_per_quality_measure)
+    datadash.create_bubble_quality() 
     
 def show_dict_from_multivalued_column(datadash:datextdash.DashboardDataExtraction, column_name: str):
     d =datadash.create_dict_from_multivalued_column(column_name)
-    mostrar_dict(d)
-        
+    show_dict(d)
+
+def show_dict_latex_from_multivalued_column(datadash:datextdash.DashboardDataExtraction, column_name: str):
+    d =datadash.create_dict_from_multivalued_column(column_name)
+    show_dict_latex(d)        
+    
+def show_dict_latex_from_single_column(datadash:datextdash.DashboardDataExtraction, column_name: str):
+    d =datadash.create_dict_from_single_column(column_name)
+    show_dict_latex(d)        
